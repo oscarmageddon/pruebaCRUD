107ddba8ae9d57f4ab0b54038d170e2738222357
Moises Neira <moises.neira@siigroup.cl>
2022-01-17 12:28:48 -0300

    Se agregan microservicios de consulta por dni y traer todos las transacciones

--- src/main/java/com/practica/ejercicio1/Controller/TransactionController.java
+++ src/main/java/com/practica/ejercicio1/Controller/TransactionController.java
@@ -1,8 +1,11 @@
 package com.practica.ejercicio1.Controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -10,6 +13,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.practica.ejercicio1.Entity.Transaction;
 import com.practica.ejercicio1.Service.TransactionService;
+
+
 
 @RestController
 @RequestMapping(value = "rest")
@@ -22,10 +27,26 @@ public class TransactionController {
 		this.transactionService = transactionService;
 	}
 	
-	@RequestMapping(value = "/tranx/", method = RequestMethod.POST)
+	@RequestMapping(value = "/tranx", method = RequestMethod.POST)
 	public ResponseEntity<Transaction> insertarTransaction(@RequestBody Transaction transaction){
 		
 		transactionService.saveTransaction(transaction);
 		return new ResponseEntity<Transaction>(transaction, HttpStatus.CREATED);
 	}
+	
+	@RequestMapping(value = "/tranx", method = RequestMethod.GET)
+	public ResponseEntity<List<Transaction>> traerTransactions () {
+		List<Transaction> transactions = transactionService.traerTransactions();
+		return new ResponseEntity<List<Transaction>>(transactions, HttpStatus.OK);
+	}
+	
+	
+	@RequestMapping(value = "/tranx/dni/{dniUsr}", method = RequestMethod.GET)
+	public ResponseEntity<Transaction> traerTransactionDni(@PathVariable("dniUsr") String dniUsr) {
+		Transaction transaction = transactionService.traerTransactionDni(dniUsr);
+		return new ResponseEntity<Transaction>(transaction, HttpStatus.CREATED);
+	}
+	
+	
+	
 }
--- src/main/java/com/practica/ejercicio1/Service/TransactionService.java
+++ src/main/java/com/practica/ejercicio1/Service/TransactionService.java
@@ -1,8 +1,10 @@
 package com.practica.ejercicio1.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import com.practica.ejercicio1.Entity.Transaction;
 import com.practica.ejercicio1.Repository.TransactionRepo;
 
@@ -19,5 +21,16 @@ public class TransactionService {
 	public void saveTransaction(Transaction transaction) {
 		repoTransaction.save(transaction);
 	}
+	
+	public List<Transaction> traerTransactions() {
+		 List<Transaction> transactions = new ArrayList<Transaction>();
+		 repoTransaction.findAll().forEach(transaction -> transactions.add(transaction));
+	     return transactions;
+	}
+	
+	public Transaction traerTransactionDni(String dni) {
+		return repoTransaction.findByDniUsr(dni);
+	}
+	
 	
 }
