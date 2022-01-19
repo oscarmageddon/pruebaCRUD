462d1d4547a58dc872daacdd704010df581be427
indira navas <indira.navas@siigroup.cl>
2022-01-19 12:02:30 -0300

    Actualizacion

--- src/main/java/com/practica/ejercicio1/Controller/TransactionController.java
+++ src/main/java/com/practica/ejercicio1/Controller/TransactionController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -33,7 +34,8 @@ public class TransactionController {
 	public TransactionController(TransactionService transactionService) {
 		this.transactionService = transactionService;
 	}
-
+    
+	@CrossOrigin(origins = "http://localhost:4200")
 	@PostMapping("/")
 	public ResponseEntity<Transaction> insertarTransaction(@RequestBody TransactionDto transactionDto)
 			throws TransactionException {
@@ -52,26 +54,29 @@ public class TransactionController {
 		}
 		return new ResponseEntity<Transaction>(transaction, HttpStatus.CREATED);
 	}
-
+	
+	@CrossOrigin(origins = "http://localhost:4200")
 	@DeleteMapping("/{id}")
 	private ResponseEntity<Transaction> deleteTransaction(@PathVariable("id") Long id) {
 		this.transactionService.deleteById(id);
 		return new ResponseEntity<Transaction>(new Transaction(), HttpStatus.OK);
 	}
 
+	@CrossOrigin(origins = "http://localhost:4200")
 	@PutMapping(value = "/{id}")
 	public ResponseEntity<Object> updateTranx(@RequestBody Transaction transaction, @PathVariable int Id) {
 		return ResponseEntity.ok(Boolean.TRUE);
 
 	}
-
+    
+	@CrossOrigin(origins = "http://localhost:4200")
 	@GetMapping("/")
 	public ResponseEntity<List<Transaction>> traerTransactions() {
 		List<Transaction> transactions = transactionService.traerTransactions();
 		return new ResponseEntity<List<Transaction>>(transactions, HttpStatus.OK);
 	}
 
-
+	@CrossOrigin(origins = "http://localhost:4200")
 	@GetMapping("/dni/{dniUsr}")
 	public ResponseEntity<Transaction> traerTransactionDni(@PathVariable("dniUsr") String dniUsr) {
 		Transaction transaction = transactionService.traerTransactionDni(dniUsr);
