538ab22715213f48d465198269306e9ede571003
Moises Neira <moises.neira@siigroup.cl>
2022-01-19 09:55:11 -0300

    fix

--- src/main/java/com/practica/ejercicio1/Service/TransactionService.java
+++ src/main/java/com/practica/ejercicio1/Service/TransactionService.java
@@ -37,19 +37,11 @@ public class TransactionService {
 	public void deleteById(Long Id) {
 		this.repoTransaction.deleteById(Id);
 	}
-<<<<<<< HEAD
-	
+
 	public void saveOrUpdate(long id, String estado) {
 		  Transaction trx = repoTransaction.getById(id);
 		  trx.setEstado(estado);
 		  repoTransaction.save(trx);
-=======
-
-	public void update(long id, String estado) {
-		Transaction trx = repoTransaction.getById(id);
-		trx.setEstado(estado);
-		repoTransaction.save(trx);
->>>>>>> master
 	}
 
 }
