af40560f0716ac6989b752ecaf7d1f40d8f81686
edgardch1 <edgard.ch.molina@gmail.com>
2022-01-27 10:24:25 -0300

    se agrega otro comentario

--- src/main/java/com/practica/ejercicio1/Repository/TransactionRepo.java
+++ src/main/java/com/practica/ejercicio1/Repository/TransactionRepo.java
@@ -4,7 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.practica.ejercicio1.Entity.Transaction;
 
-
+// E.C 
 public interface TransactionRepo extends JpaRepository<Transaction, Long>{
 	
 	public Transaction findByDniUsr(String dni);
